b9fc85db7cc2712b6c6880cb12d4af91991a19b0
Dinh Tung <tung189cv@gmail.com>
2023-06-28 20:01:26 +0900

    add migrate model blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_28_102936) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_28_110000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
