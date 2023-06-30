f6210ac1f1ac2a9cc7cef0e3f25caad3b385838b
Dinh Tung <tung189cv@gmail.com>
2023-06-30 19:46:20 +0900

    crud and mailler

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_save :send_invite_notification
+
+  def send_invite_notification
+    binding.pry
+    UserInvitationJob.perform_later self
+  end
 end
